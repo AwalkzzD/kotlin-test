333510f39040b5be9d95f800a4263d1fd3538246
Devarshi Patel <devarshi.patel@drcsystems.com>
2024-01-30 18:03:43 +0530

    ui modification

--- app/src/main/java/com/example/myapplication/fragments/ViewsFragment.kt
+++ app/src/main/java/com/example/myapplication/fragments/ViewsFragment.kt
@@ -1,5 +1,6 @@
 package com.example.myapplication.fragments
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -7,6 +8,7 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import com.example.myapplication.R
+import com.example.myapplication.ToDoActivity
 import com.example.myapplication.databinding.FragmentViewsBinding
 
 
@@ -23,8 +25,7 @@ class ViewsFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
 
         binding.todoBtn.setOnClickListener {
-//            startActivity(Intent(activity, ToDoActivity::class.java))
-            findNavController().navigate(R.id.action_viewsFragment_to_paginationExampleFragment)
+            startActivity(Intent(activity, ToDoActivity::class.java))
         }
 
         binding.alarmBtn.setOnClickListener {
@@ -37,6 +38,10 @@ class ViewsFragment : Fragment() {
 
         binding.tabLayoutBtn.setOnClickListener {
             findNavController().navigate(R.id.action_viewsFragment_to_tabLayoutFragment)
+        }
+
+        binding.pagingBtn.setOnClickListener {
+            findNavController().navigate(R.id.action_viewsFragment_to_paginationExampleFragment)
         }
     }
 }
